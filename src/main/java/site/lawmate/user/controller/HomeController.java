6d621bf8e9246f39c9866dd0c1fd9eeca5b3c829
iuhapark <hanied04@gmail.com>
2024-06-25 01:09:45 +0900

    Feat - google authorization page routing

--- src/main/java/site/lawmate/user/controller/HomeController.java
+++ src/main/java/site/lawmate/user/controller/HomeController.java
@@ -1,19 +1,36 @@
 package site.lawmate.user.controller;
 
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import site.lawmate.user.domain.dto.SessionUser;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @RestController
+@RequiredArgsConstructor
 public class HomeController {
 
     public String Date = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date());
+
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String home() {
         return "Welcome to user server! " + Date;
     }
+
+    @GetMapping("/google")
+    public String auth(Model model) {
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+        return "google";
+    }
+
 
 }
