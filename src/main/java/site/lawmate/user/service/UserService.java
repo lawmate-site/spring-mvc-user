e7e86e59a0af43493a2279f59c53706e5d1f329c
iuhapark <hanied04@gmail.com>
2024-06-25 18:31:12 +0900

    Feat - user CRUD

--- src/main/java/site/lawmate/user/service/UserService.java
+++ src/main/java/site/lawmate/user/service/UserService.java
@@ -5,41 +5,41 @@ import site.lawmate.user.component.Messenger;
 import site.lawmate.user.domain.model.User;
 import site.lawmate.user.domain.dto.UserDto;
 
-public interface UserService extends UserDetailsService {
+public interface UserService extends UserDetailsService, CommandService<UserDto>, QueryService<UserDto> {
 
-//    default User dtoToEntity(UserDto dto) {
-//        return User.builder()
-//                .email(dto.getEmail())
-//                .name(dto.getName())
-//                .phone(dto.getPhone())
-//                .age(dto.getAge())
-//                .sex(dto.getSex())
-//                .point(dto.getPoint())
-//                .password(dto.getPassword())
-//                .build();
-//    }
-//
-//    default UserDto entityToDto(User user) {
-//        return UserDto.builder()
-//                .id(user.getId())
-//                .email(user.getEmail())
-//                .name(user.getName())
-//                .phone(user.getPhone())
-//                .age(user.getAge())
-//                .sex(user.getSex())
-//                .point(user.getPoint())
-//                .password(user.getPassword())
-//                .build();
-//    }
-//
-//    Messenger login(UserDto param);
-//
-//    Boolean logout(String accessToken);
-//
-//    Messenger modify(UserDto user);
-//
-//    User autoRegister();
-//
-//    Boolean existsByEmail(String email);
+    default User dtoToEntity(UserDto dto) {
+        return User.builder()
+                .email(dto.getEmail())
+                .name(dto.getName())
+                .phone(dto.getPhone())
+                .age(dto.getAge())
+                .sex(dto.getSex())
+                .point(dto.getPoint())
+                .password(dto.getPassword())
+                .build();
+    }
+
+    default UserDto entityToDto(User user) {
+        return UserDto.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .name(user.getName())
+                .phone(user.getPhone())
+                .age(user.getAge())
+                .sex(user.getSex())
+                .point(user.getPoint())
+                .password(user.getPassword())
+                .build();
+    }
+
+    Messenger login(UserDto param);
+
+    Boolean logout(String accessToken);
+
+    Messenger modify(UserDto user);
+
+    User autoRegister();
+
+    Boolean existsByEmail(String email);
 
 }
