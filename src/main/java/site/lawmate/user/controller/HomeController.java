aa1ff32907d3db1aadb8cb8c990ba72607838a9d
iuhapark <hanied04@gmail.com>
2024-06-28 13:16:48 +0900

    ""

--- src/main/java/site/lawmate/user/controller/HomeController.java
+++ src/main/java/site/lawmate/user/controller/HomeController.java
@@ -1,11 +1,8 @@
 package site.lawmate.user.controller;
 
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import site.lawmate.user.domain.dto.SessionUser;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -16,22 +13,9 @@ public class HomeController {
 
     public String Date = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date());
 
-    private final HttpSession httpSession;
-
     @GetMapping("/")
     public String home() {
         return "Welcome to user server! " + Date;
     }
 
-    @GetMapping("/auth")
-    public String auth(Model model) {
-//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-//        if (user != null) {
-//            model.addAttribute("userName", user.getName());
-//            return "Hello, " + user.getName() + "!";
-//        } else if (user == null) {
-//            return "Hello, Guest!";
-//        }
-        return "Hello, Guest!";
-    }
 }
