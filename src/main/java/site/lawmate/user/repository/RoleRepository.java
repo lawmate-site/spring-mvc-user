56a0e5d2355558d91e600b8af962f74d68311379
iuhapark <hanied04@gmail.com>
2024-08-01 19:18:21 +0900

    Remove

--- src/main/java/site/lawmate/user/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package site.lawmate.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import site.lawmate.user.domain.model.RoleModel;
-
-@Repository
-public interface RoleRepository extends JpaRepository<RoleModel, Long> {
-}
